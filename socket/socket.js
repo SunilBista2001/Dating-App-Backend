788090c5488f57b303e365c43f5a1acc895ba5ef
Sunil <btwitsbista@gmail.com>
2024-04-17 18:30:24 +0545

    fixes socket errors

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    // origin: ["http://localhost:3000"],
+    origin: "*", // Allow requests from any origin
     methods: ["GET", "POST"],
   },
 });
